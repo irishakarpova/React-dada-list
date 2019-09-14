af44ac39bfa94bb15a5ee6fac8e838bdf7707716
irina <irisha.karpova@gmail.com>
2019-09-14 17:13:42 -0500

    toggleDecorator

--- src/decorators/toggleDecorator.js
+++ src/decorators/toggleDecorator.js
@@ -1,20 +1,29 @@
 import React from 'react'
-export default (OriginalComponent) => class ToggleComponent extends React.Component{
-  
+export default (OriginalComponent) => class ToggleIconComponent extends React.Component{
+
   state={
-    isOpen: false
+	opacity: 1,
+	transformL: 'rotate(0deg)',
+	transformR: 'rotate(0deg)',
+	padding:'0 0 0 0'
   }
-  handleClick = () =>{
+
+  toggleAnimation=()=>{
     this.setState({
-      isOpen: !this.state.isOpen
+      padding: this.state.padding === '0 0 0 0' ? '0 0 0 50%' : '0 0 0 0',
+      transformL: this.state.transformL === 'rotate(0deg)' ? 'rotate(-45deg)': 'rotate(0deg)',
+      transformR: this.state.transformR === 'rotate(0deg)' ? 'rotate(45deg)': 'rotate(0deg)',
+	    opacity: this.state.opacity === 0 ? 1 : 0
     })
   }
 
   render(){
     return <OriginalComponent
-      {...this.props}
-      isOpen = {this.state.isOpen}
-      handleClick = {this.handleClick}
+      opacity = {this.state.opacity}
+  	  transformL = {this.state.transformL}
+  	  transformR = {this.state.transformR}
+  	  padding = {this.state.padding}
+  	  toggleAnimation = {this.toggleAnimation}
     />
   }
 }
